af0dc440b2ac1cc42236e6423196379cd6350f59
Patryk Olewniak <p.olewniak@gmail.com>
2020-02-23 19:55:24 +0100

    + Template with CSS

---
+++ files/reportTemplate.py
@@ -0,0 +1,20 @@
+template ="""
+<html>
+<head>
+<link rel="stylesheet" href="files/reportStyles.css">
+<title>TEST REPORT</title>
+</head>
+<body>
+<table align="center" class="colors">
+  <tr>
+    <th><font color="BLUE">BLUE = INSERTION OR DELETION</font></th>
+    <th><B>BOLD = SUBSTITUTION</font></B></th>
+    <th><font color="GREY">GREY = The same elements before or after</font></th>
+  </tr>
+</table>
+<HR>
+<BR>
+%s
+</body>
+</html>
+"""
